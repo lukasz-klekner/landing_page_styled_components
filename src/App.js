4a269c3f7bb3f81fb435d3db11b27d31f5fe849e
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-16 12:58:54 +0200

    creates theme and GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './GlobalStyles'
+import { theme } from './theme'
+
 function App() {
-  return <div>test</div>
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div>Hello React & styled-components</div>
+      </>
+    </ThemeProvider>
+  )
 }
 
 export default App
